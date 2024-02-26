2ed8aaa8f1df375f738231c89ea0a49825932edf
RobiHardinata19 <robi.120140034@student.itera.ac.id>
2024-02-26 18:56:28 +0700

    first commit

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,5 @@
+@vite('resources/css/app.css')
+
+<div class="text-3xl mt-2 bg-blue-400 rounded-lg">
+    {{$slot}}
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ini Home</h1>
+    <x-card>
+        satu
+    </x-card>
+    <x-card>
+        dua
+    </x-card>
+</body>
+</html>
